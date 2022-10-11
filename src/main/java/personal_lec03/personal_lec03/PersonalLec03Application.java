93e1109ff374ddf71fae3ead898916be114bb41b
B20037-Kobayashi <e1b20037@oit.ac.jp>
2022-10-11 20:06:21 +0900

    初期化

---
+++ src/main/java/personal_lec03/personal_lec03/PersonalLec03Application.java
@@ -0,0 +1,13 @@
+package personal_lec03.personal_lec03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalLec03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalLec03Application.class, args);
+	}
+
+}
---
+++ src/test/java/personal_lec03/personal_lec03/PersonalLec03ApplicationTests.java
@@ -0,0 +1,13 @@
+package personal_lec03.personal_lec03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalLec03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
