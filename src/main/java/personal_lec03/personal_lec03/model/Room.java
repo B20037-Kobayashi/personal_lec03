b1ab6c5119b635147868a39133e6b9185b5bc5a7
B20037-Kobayashi <e1b20037@oit.ac.jp>
2022-10-13 18:25:12 +0900

    Sample3-8の実施

---
+++ src/main/java/personal_lec03/personal_lec03/model/Room.java
@@ -0,0 +1,36 @@
+package personal_lec03.personal_lec03.model;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Room {
+  ArrayList<String> users = new ArrayList<>();
+  int roomNo = 1;
+
+  public void addUser(String name) {
+    for (String s : this.users) {
+      if (s.equals(name)) {
+        return;
+      }
+    }
+    this.users.add(name);
+  }
+
+  public int getRoomNo() {
+    return roomNo;
+  }
+
+  public void setRoomNo(int roomNo) {
+    this.roomNo = roomNo;
+  }
+
+  public ArrayList<String> getUsers() {
+    return users;
+  }
+
+  public void setUsers(ArrayList<String> users) {
+    this.users = users;
+  }
+}
